9c9b34fed0506c51d94a3c60f78b002c8dfc8468
2024108060008 <leticia.danielly@estudante.ifro.edu.br>
2026-09-23 08:05:42 -0400

    Estrutara de codigo de HTML da tela de Login e outras configurações

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from usuarios.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cardapio.urls')),
-    path('usuarios/', include('usuarios.urls')),
+    path('login/', login_view, name='login'),
     path('avaliacoes/', include('avaliacoes.urls')),
     path('administracao/', include('administracao.urls')),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Quando essa função for chamada, vai mostrar a página login.html
+def login_view(request):
+    return render(request, 'usuarios/login.html')
